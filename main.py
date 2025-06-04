2c327d7142240f217a7ae0963f6edf9ee404786e
Tamilselvan Murugesan <tamilselvanmurugesan@Tamilselvans-MacBook-Pro.local>
2025-06-04 23:52:54 +0530

    json data update

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# Sample employee data (could be from a database in real apps)
+employees = [
+    {"id": 1, "name": "Alice", "email":"alice@gmail.com", "position":"Senior Software Dev", "department": "Engineering"},
+    {"id": 2, "name": "Bob", "email":"bob@fmail.com", "position":"BA", "department": "Marketing"},
+    {"id": 3, "name": "Charlie", "email":"charlie.98@yahoo.in", "position":"Associate", "department": "HR"}
+    
+]
+
+@app.route('/employees', methods=['GET'])
+def get_employees():
+    return jsonify(employees)
+
+if __name__ == '__main__':
+    app.run(debug=True)
