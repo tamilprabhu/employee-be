c36bb08d3ef800d5b1ffda037941d8d2bf8b43bd
Tamilselvan Murugesan <tamilselvanmurugesan@Tamilselvans-MacBook-Pro.local>
2025-06-12 01:22:30 +0530

    mysql connection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+import mysql.connector
 
 app = Flask(__name__)
 CORS(app)
@@ -12,9 +13,27 @@ employees = [
     
 ]
 
+conn = mysql.connector.connect(
+    host = '127.0.0.1:3306',
+    user = 'root',
+    password = 'root',
+    database = 'employee'
+)
+
 @app.route('/employees', methods=['GET'])
 def get_employees():
     return jsonify(employees)
+
+@app.route('/getData', methods=['GET'])
+def get_tables():
+    cursor = conn.cursor()
+    cursor.execute("SHOW TABLES;")
+    tables = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    table_names = [table[0] for table in tables]
+    return jsonify({"tables":table_names}), 200
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
