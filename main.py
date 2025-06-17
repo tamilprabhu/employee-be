3f28c334c0af19a0a4033f455ad02e044f10aec7
Tamilselvan Murugesan <tamilselvanmurugesan@Tamilselvans-MacBook-Pro.local>
2025-06-17 13:21:50 +0530

    certificate install

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ import mysql.connector
 app = Flask(__name__)
 CORS(app)
 
+# conn = mysql.connector.connect(
+#     host = 'localhost',
+#     port = '8080',
+#     user = 'root',
+#     password = 'root',
+#     database = 'employee'
+# )
+
 # Sample employee data (could be from a database in real apps)
 employees = [
     {"id": 1, "name": "Alice", "email":"alice@gmail.com", "position":"Senior Software Dev", "department": "Engineering"},
@@ -12,28 +20,30 @@ employees = [
     {"id": 3, "name": "Charlie", "email":"charlie.98@yahoo.in", "position":"Associate", "department": "HR"}
 ]
 
-conn = mysql.connector.connect(
-    host = '127.0.0.1',
-    port = '3306',
-    user = 'root',
-    password = 'root',
-    database = 'employee'
-)
-
 @app.route('/employees', methods=['GET'])
 def get_employees():
     return jsonify(employees)
 
 @app.route('/getData', methods=['GET'])
 def get_tables():
-    cursor = conn.cursor()
-    cursor.execute("SHOW TABLES;")
-    tables = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    table_names = [table[0] for table in tables]
-    return jsonify({"tables":table_names}), 200
+    try:
+        conn = mysql.connector.connect(
+            host='127.0.0.1',
+            port=3306,
+            user='root',
+            password='root',
+            database='employee'
+        )
+        cursor = conn.cursor()
+        cursor.execute("SHOW TABLES;")
+        tables = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        table_names = [table[0] for table in tables]
+        return jsonify({"tables": table_names}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
     
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, ssl_context=('cert/cert.pem', 'cert/key.pem'))
